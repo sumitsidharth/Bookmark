2546bb935e404d3d897af2652df5c4b58633ee46
Sumit Sidharth <sumitsidharth30@gmail.com>
2026-05-14 19:07:59 +0530

    Handle Redis connection errors gracefully for PythonAnywhere

--- images/views.py
+++ images/views.py
@@ -60,10 +60,16 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    # increment total image views by 1
-    total_views = r.incr(f'image:{image.id}:views')
-    # increment image ranking by 1
-    r.zincrby('image_ranking', 1, image.id)
+    total_views = 0
+    try:
+        # increment total image views by 1
+        total_views = r.incr(f'image:{image.id}:views')
+        # increment image ranking by 1
+        r.zincrby('image_ranking', 1, image.id)
+    except redis.exceptions.ConnectionError:
+        # Redis is not available (e.g., on PythonAnywhere Free)
+        pass
+        
     return render(
         request,
         'images/image/detail.html',
@@ -131,19 +137,25 @@ def image_list(request):
 
 @login_required
 def image_ranking(request):
-    # get image ranking dictionary
-    image_ranking = r.zrange(
-        'image_ranking', 0, -1,
-        desc=True
-    )[:10]
-    image_ranking_ids = [int(id) for id in image_ranking]
-    # get most viewed images
-    most_viewed = list(
-        Image.objects.filter(
-            id__in=image_ranking_ids
+    most_viewed = []
+    try:
+        # get image ranking dictionary
+        image_ranking = r.zrange(
+            'image_ranking', 0, -1,
+            desc=True
+        )[:10]
+        image_ranking_ids = [int(id) for id in image_ranking]
+        # get most viewed images
+        most_viewed = list(
+            Image.objects.filter(
+                id__in=image_ranking_ids
+            )
         )
-    )
-    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+        most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    except redis.exceptions.ConnectionError:
+        # Redis is not available
+        pass
+        
     return render(
         request,
         'images/image/ranking.html',
